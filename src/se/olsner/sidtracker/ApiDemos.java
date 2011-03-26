09779e6b8904e4578b71f1f2ca8042be80a4aecf
Simon Brenner <olsner@gmail.com>
2011-03-26 23:03:03 +0100

    Add some example register settings and run the output loop properly

--- src/se/olsner/sidtracker/ApiDemos.java
+++ src/se/olsner/sidtracker/ApiDemos.java
@@ -12,13 +12,51 @@ public class ApiDemos extends Activity {
 		public void run() {
 			SID sid = new SID();
 			AudioTrack output = new AudioTrack(AudioManager.STREAM_MUSIC, sid.getSampleRate(), AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, 22050, AudioTrack.MODE_STREAM);
-			// TODO Set up some nice test contents in the registers...
-			short[] buffer = new short[44100];
+			output.play();
+			System.err.println("output state is "+output.getState()+", play state is "+output.getPlayState());
+			
+			sid.write(24, 15); // volume = max
+			sid.write(5, 0); // attack/decay
+			sid.write(6, 0xf0); // sustain/release
+
+			sid.write(0, 0xd6); // note, low byte
+			sid.write(1, 0x1c); // note, high byte
+
+			byte gate = 16;
+			short[] buffer = new short[5012];
+			int endOfBuffer = 0;
+			long samples = 0;
 			while (true)
 			{
-				sid.clockFully(buffer);
-				output.write(buffer, 0, buffer.length);
+				if (samples > 44100) // 1s
+				{
+					sid.write(4, gate ^= 1);
+					samples -= 44100;
+				}
+				double time0 = System.nanoTime();
+				sid.clockFully(buffer, endOfBuffer, buffer.length - endOfBuffer);
+				double time1 = System.nanoTime();
+				System.err.println(""+(buffer.length - endOfBuffer)+" samples generated in "+nano2s(time1 - time0)+"s");
+				int outputted = output.write(buffer, 0, buffer.length);
+				samples += outputted;
+				double time2 = System.nanoTime();
+				System.err.println(""+outputted+" samples sent to output in "+nano2s(time2 - time1)+"s");
+				if (outputted < buffer.length)
+				{
+					int n = 0;
+					for (int i = outputted; i < buffer.length; i++)
+						buffer[n++] = buffer[i];
+					endOfBuffer = n;
+				}
+				else
+				{
+					endOfBuffer = 0;
+				}
 			}
+		}
+
+		private double nano2s(double d) {
+			return d / 1000000000;
 		}
 	}
 
