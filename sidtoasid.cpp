32e8be08a32f3ab5ad3d67d8c90bfe9cc94d199b
Simon Brenner <olsner@gmail.com>
2012-06-24 21:59:30 +0200

    Introduce a binary file format for SID conversion tool

--- sidtoasid.cpp
+++ sidtoasid.cpp
@@ -1,8 +1,67 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
 #include <sidplay/sidplay2.h>
+
+/* Notes on data density: almost all timestamp deltas fit in one byte, some
+ * fairly common offsets up to ~19200 (0xff*75 + 62), then some outliers.
+ * Largest time-delta: 40110.
+ *
+ * Offsets 1 through 3 never appear in $randomsidtune.
+ */
+struct deltafile
+{
+	int fd;
+
+	deltafile(const char* filename)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+		assert(fd >= 0);
+	}
+
+	~deltafile()
+	{
+		close(fd);
+	}
+
+	void write(uint8_t byte)
+	{
+		int res = ::write(fd, &byte, 1);
+		assert(res == 1);
+	}
+
+	void writeInt(unsigned long value)
+	{
+		if (value < 0xfe)
+		{
+			write(value);
+		}
+		else if (value <= 0xffff)
+		{
+			write(0xfe);
+			writeInt16(value);
+		}
+		else
+		{
+			write(0xff);
+			writeInt32(value);
+		}
+	}
+
+	void writeInt16(unsigned long value)
+	{
+		write((value >> 8) & 0xff);
+		write(value & 0xff);
+	}
+
+	void writeInt32(unsigned long value)
+	{
+		writeInt16((value >> 16) & 0xffff);
+		writeInt16(value & 0xffff);
+	}
+};
 
 void usage(FILE* out)
 {
@@ -14,10 +73,15 @@ struct asidemu: public sidemu
 {
 	uint8_t regs[256];
 	c64env* env;
+	unsigned long lastWrite;
+	deltafile outfile;
 
-	asidemu(sidbuilder* builder, c64env* env, sid2_model_t model):
+	asidemu(const char* outfile,
+			sidbuilder* builder, c64env* env, sid2_model_t model):
 		sidemu(builder),
-		env(env)
+		env(env),
+		outfile(outfile),
+		lastWrite(0)
 	{
 		reset(0);
 		printf("init: model := %d\n", model);
@@ -38,8 +102,13 @@ struct asidemu: public sidemu
 	void write(uint8_t addr, uint8_t data)
 	{
 		writes++;
-		fprintf(stderr, "%lu: %#x := %#x\n", (long unsigned)getTime(), addr, data);
+		unsigned long now = getTime();
+		//fprintf(stderr, "+%lu %#x := %#x\n", now - lastWrite, addr, data);
+		outfile.writeInt(now - lastWrite);
+		uint8_t delta = regs[addr] ^ data;
+		outfile.write(delta);
 		regs[addr] = data;
+		lastWrite = now;
 	}
 
 	event_clock_t getTime() const
@@ -61,15 +130,25 @@ struct asidemu: public sidemu
 
 struct asidbuilder: public sidbuilder
 {
-	asidbuilder(): sidbuilder("aSID converter") {}
+	const char* outfile;
+	bool locked;
+
+	asidbuilder(const char* outfile):
+		sidbuilder("aSID converter"),
+		outfile(outfile),
+		locked(false)
+	{}
 
 	sidemu* lock(c64env* env, sid2_model_t model)
 	{
-		return new asidemu(this, env, model);
+		assert(!locked);
+		return new asidemu(outfile, this, env, model);
 	}
 	void unlock(sidemu* device)
 	{
+		assert(locked);
 		delete device;
+		locked = false;
 	}
 
 	const char* credits() { return "SID to SID* conversion, by Simon Brenner"; }
@@ -102,7 +181,8 @@ int main(int argc, const char* argv[])
 	cfg.optimisation = SID2_DEFAULT_OPTIMISATION;
 	cfg.sidSamples = true;
 	cfg.playback = sid2_mono;
-	asidbuilder builder;
+
+	asidbuilder builder(outfile);
 	cfg.sidEmulation = &builder;
 	int i = player.config(cfg);
 	if (i < 0)
