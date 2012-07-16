45c9ac770c014a17b12eb9ee297e27020fe4e789
Simon Brenner <olsner@gmail.com>
2012-07-16 21:51:08 +0200

    Integrate ndk build into the ant script

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,8 +6,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := sidtracker
 ### Add all source file names to be included in lib separated by a whitespace
 LOCAL_SRC_FILES := sidtracker.cpp
-LOCAL_CFLAGS := -fvisibility=hidden -fvisibility-inlines-hidden -Wall -O3
-LOCAL_LDLIBS := -llog -Wl,--version-script,$(MY_JNI_DIR)/version-script.txt
+LOCAL_CFLAGS := -fvisibility=hidden -fvisibility-inlines-hidden -Wall
+LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS += -Wl,--version-script,$(MY_JNI_DIR)/version-script.txt
 # LOCAL_LDLIBS += -Wl,-Map,linker.map
 LOCAL_ARM_MODE := arm
 PYTHON ?= python
