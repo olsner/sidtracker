1701d92b2812d63c7f17cdb90f8cf9e9cca795c2
Simon Brenner <olsner@gmail.com>
2012-07-17 19:47:44 +0200

    Remove unused code

--- src/se/olsner/sidtracker/SIDTracker.java
+++ src/se/olsner/sidtracker/SIDTracker.java
@@ -48,32 +48,6 @@ public class SIDTracker extends Activity {
 		}
 	}
 	
-	class SIDThread implements Runnable {
-		private SoundQueue queue;
-		private SID sid;
-		
-		public SIDThread(SID sid, SoundQueue queue) {
-			this.queue = queue;
-			this.sid = sid;
-		}
-
-		public void run() {
-			while (queue.isLive())
-			{
-				short[] buffer = queue.getBufferFromPool();
-				if (buffer == null) buffer = new short[441]; // 1/100th of a second... Maybe we should have less though?
-				sid.clockFully(buffer, 0, buffer.length);
-				postBuffer(buffer);
-			}
-			
-			Log.d("SIDThread", "SID thread done!");
-		}
-
-		private void postBuffer(short[] buffer) {
-			queue.postRunningControlMessages(buffer);
-		}
-	}
-
 	static {
 		System.loadLibrary("sidtracker");
 	}
@@ -88,66 +62,8 @@ public class SIDTracker extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		//createInteractiveMode();
 		//createFollowMode();
 	}
-
-	/*private void createInteractiveMode() {
-		sidControl = new SIDControl(sid, new SoundQueue());
-		initSID();
-
-		ToggleButton gateToggle = (ToggleButton)findViewById(R.id.toggleGATE);
-		gateToggle.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				sidControl.setGate(0, isChecked);
-			}
-		});
-		
-		final View touchView = findViewById(R.id.touchInputView);
-		touchView.setOnTouchListener(new OnTouchListener() {
-			int lastFilter = -1, lastFreq = -1;
-			boolean lastGate = false;
-			
-			public boolean onTouch(View v, MotionEvent event) {
-				int filterMin = SID.MAX_FILTER_CUTOFF / 3, filterMax = SID.MAX_FILTER_CUTOFF;
-				int freqMin = 100, freqMax = SID.MAX_FREQUENCY;
-				int x = (int)event.getX();
-				// Flip Y to make up = higher
-				int y = v.getHeight() - (int)event.getY();
-				int filter = filterMin + (filterMax - filterMin) * x / v.getWidth();
-				int freq = freqMin + (freqMax - freqMin) * y / v.getHeight();
-				if (lastFilter != filter)
-				{
-					sidControl.setFilterCutoff(filter);
-					lastFilter = filter;
-				}
-				if (lastFreq != freq)
-				{
-					sidControl.setFrequencyHz(0, freq);
-					lastFreq = freq;
-				}
-				boolean gate = getPointerStateFromAction(event.getActionMasked());
-				if (lastGate != gate) {
-					sidControl.setGate(0, gate);
-					lastGate = gate;
-				}
-				return true;
-			}
-
-			private boolean getPointerStateFromAction(int actionMasked) {
-				switch (actionMasked)
-				{
-				case MotionEvent.ACTION_DOWN:
-				case MotionEvent.ACTION_MOVE:
-				case MotionEvent.ACTION_POINTER_DOWN:
-					return true;
-				default:
-					return false;
-				}
-			}
-		});
-	}*/
 
 	private void createFollowMode() {
 		final View touchView = findViewById(R.id.touchInputView);
@@ -205,20 +121,6 @@ public class SIDTracker extends Activity {
 				});
 			}
 		});
-	}
-
-	private void initSID() {
-		Log.i("SIDTracker", "SID Initialized: "+sid);
-		/*sid.write(23, (7 << 4) | 7); // resonancy = 7, enable filter for all channels
-		sid.write(24, 16 | 15); // enable lowpass filter, volume = 15
-		sid.write(5, 0); // attack/decay
-		sid.write(6, 0xf0); // sustain/release
-
-		sid.write(0, 0xd6); // note, low byte
-		sid.write(1, 0x1c); // note, high byte
-
-		sid.write(4, 32); // gate/waveform: triangle wave, gate=0
-		*/
 	}
 
 	InputStream openAsset(String path) throws IOException {
